2937c845a5718e5ccc320e2cb9381086decbed2d
Evan Wilde <etcwilde@uvic.ca>
2015-03-30 14:09:06 -0700

    new main that tests intersection and union operations

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,19 @@ int main()
 	Implicit::Primitive p(geoffFunction, 0.5, 1);
 	Implicit::Translate t1(&p, 0, -0.2, 0);
 	Implicit::Translate t2(&p, 0, 0.2, 0);
-	Implicit::Blend b(&t1, &t2, 0.5);
-	//Implicit::Rotate r(&b, glm::vec3(0, 0, 1), M_PI /2.f);
-	Implicit::Scale s(&b, 0.24, 0.25, 1);
-	Implicit::Rotate r(&s, 0, M_PI / 3.f , 0);
-	Implicit::Translate t(&r ,0, 0 , -3);
+	Implicit::Translate t3(&p, -0.8, 0, 0);
+	Implicit::Translate t4(&p, 0.8, 0, 0);
+
+	Implicit::Union u1(&t3, &t4);
+
+	Implicit::Intersect i1(&t1, &t2);
+
+	Implicit::Intersect u2(&i1, &u1);
+
+
+	Implicit::Translate t(&u2 ,0, 0 , -3);
 	Image image(960, 480);
-	RayTracer R(&t, &image, 1);
+	RayTracer R(&t, &image, 8);
 	auto begin = std::chrono::high_resolution_clock::now();
 	R.Trace(0, image.width(), 0, image.height());
 	auto end = std::chrono::high_resolution_clock::now();
